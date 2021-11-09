1403e21e6dc156391ab92ec485ae9b459af22b86
FireboltAA <noman.gronckle@gmail.com>
2021-11-08 22:20:30 -0800

    convert rand to functions

--- practice/dice_roller.jl
+++ practice/dice_roller.jl
@@ -1,31 +1,92 @@
 i = 1 # 'i' is used in while loop
-
-printstyled("What type of dice do you want to roll?: "; color = :green) # Get type of dice from user
-type = readline()
+upper = 0
+ttl = 0
 
 printstyled("How many dice would you like to roll?: "; color = :blue) # Get amount of dice as string from user
 amount_string = readline()
 
 amount = parse(Int64, amount_string) # Parse amount to Int64
-println(amount)
+
+printstyled("What type of dice do you want to roll?: "; color = :green) # Get type of dice from user
+type = readline()
 
 
-while (i < amount)
+
+function rollD4()
+    upper = 4
+    lower = 1
+    global num = rand(1:4)
+    println(num)
+    return num
+end
+
+function rollD6()
+    upper = 6
+    lower = 1
+    global num = rand(1:6)
+    println(num)
+    return num
+end
+
+function rollD8()
+    upper = 8
+    lower = 1
+    global num = rand(1:8)
+    println(num)
+    return num
+end
+
+function rollD10()
+    upper = 10
+    lower = 1
+    global num = rand(1:10)
+    println(num)
+    return num
+end
+
+function rollD12()
+    upper = 12
+    lower = 1
+    global num = rand(1:12)
+    println(num)
+    return num
+end
+
+function rollD20()
+    upper = 20
+    lower = 1
+    global num = rand(1:20)
+    println(num)
+    return num
+end
+
+
+
+while (i <= amount)
     if (type == "d4")
-        println(rand(1:4))
+        rollD4()
+        global ttl = ttl + num
     elseif (type == "d6")
-        println(rand(1:6))
+        rollD6()
+        global ttl = ttl + num
     elseif (type == "d8")
-        println(rand(1:8))
+        rollD8()
+        global ttl = ttl + num
     elseif (type == "d10")
-        println(rand(1:10))
+        rollD10()
+        global ttl = ttl + num
     elseif (type == "d12")
-        println(rand(1:12))
+        rollD12()
+        global ttl = ttl + num
     elseif (type == "d20")
-        println(rand(1:20))
+        rollD20()
+        global ttl = ttl + num
     else
         printstyled("~Invalid Type Error~"; color = :red)
         println()
     end
     global i += 1
 end
+
+printstyled("Total: "; color = :green)
+println(ttl)
