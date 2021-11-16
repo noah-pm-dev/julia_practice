37a65dd321569bd47ae3c2835425132540559681
FireboltAA <noman.gronckle@gmail.com>
2021-11-15 21:56:01 -0800

    Optimize literally everything my god I'm stupid

--- practice/dice_roller.jl
+++ practice/dice_roller.jl
@@ -17,7 +17,7 @@ else
     exit(1)
 end
 
-dieSplit = replace(die, "d" => "+d") # Insert character to use later for splitting string into substrings
+dieSplit = replace(die, "d" => "+d+") # Insert character to use later for splitting string into substrings
 
 stringSplit = split(dieSplit, "+") # Split string at splitter into substrings
 
@@ -33,27 +33,18 @@ end
 
 amount = parse(Int64, amountString) # Parse amountString substring to Int64
 
-type = stringSplit[2] # Assign substring at index 2 to new variable
+maxString = stringSplit[3] # Assign substring at index 3 to new variable
+max = parse(Int64, maxString)
 ##
-
-# Check for invalid type
-if (occursin("4", type) == true || occursin("6", type) == true || occursin("8", type) == true || occursin("10", type) == true || occursin("12", type) == true || occursin("20", type) == true || occursin("100", type) == true)
-    return
-else
-    printstyled("That is not a valid type of die! Valid types are {d4, d6, d8, d10, d12, d20, or d100}\n"; color = :red)
-    println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    exit(1)
-end
 
 
 
 
 ## Number generating and counting # of max rolls
 
-function rollD4()
-    upper = 4
-    lower = 1
-    global num = rand(1:4)
+function roll(upper)
+    maximum = upper
+    global num = rand(1:upper)
     println(num)
     if (num == upper)
         global numOfMax += 1
@@ -61,104 +52,11 @@ function rollD4()
     return num
 end
 
-function rollD6()
-    upper = 6
-    lower = 1
-    global num = rand(1:6)
-    println(num)
-    if (num == upper)
-        global numOfMax += 1
-    end
-    return num
-end
-
-function rollD8()
-    upper = 8
-    lower = 1
-    global num = rand(1:8)
-    println(num)
-    if (num == upper)
-        global numOfMax += 1
-    end
-    return num
-end
-
-function rollD10()
-    upper = 10
-    lower = 1
-    global num = rand(1:10)
-    println(num)
-    if (num == upper)
-        global numOfMax += 1
-    end
-    return num
-end
-
-function rollD12()
-    upper = 12
-    lower = 1
-    global num = rand(1:12)
-    println(num)
-    if (num == upper)
-        global numOfMax += 1
-    end
-    return num
-end
-
-function rollD20()
-    upper = 20
-    lower = 1
-    global num = rand(1:20)
-    println(num)
-    if (num == upper)
-        global numOfMax += 1
-    end
-    return num
-end
-
-function rollD100()
-    upper = 100
-    lower = 1
-    global num = rand(1:100)
-    println(num)
-    if (num == upper)
-        global numOfMax += 1
-    end
-    return num
-end
-##
-
-# Main loop
+## Main loop
 
 while (i <= amount)
-    if (type == "d4")
-        rollD4()
-        global ttl = ttl + num
-    elseif (type == "d6")
-        rollD6()
-        global ttl = ttl + num
-    elseif (type == "d8")
-        rollD8()
-        global ttl = ttl + num
-    elseif (type == "d10")
-        rollD10()
-        global ttl = ttl + num
-    elseif (type == "d12")
-        rollD12()
-        global ttl = ttl + num
-    elseif (type == "d20")
-        rollD20()
-        global ttl = ttl + num
-    elseif (type == "d100")
-	rollD100()
-	global ttl = ttl + num
-    else
-        printstyled("~Invalid Type Error~"; color = :red)
-        println()
-        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        exit(1)
-    end
-    global i += 1
+    roll(max)
+    global i += i
 end
 ##
 
