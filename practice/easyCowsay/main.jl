58f60a5064881c2ad644b720fb526c3f9aca2391
FireboltAA <noman.gronckle@gmail.com>
2021-12-02 15:22:59 -0800

    comments and extra clearing

--- practice/easyCowsay/main.jl
+++ practice/easyCowsay/main.jl
@@ -2,18 +2,19 @@ using REPL.TerminalMenus
 
 ## Single Letter Variable List
 # a: requested choice for charList, used in charList[a]
-#
+# b: requested choice for fortuneQuery, used in fortuneQuery[b]
 #
 
 fortuneQuery = ["fortune", "custom text"]
 
 charList = ["apt", "frogs", "bunny", "calvin", "cheese", "rooster", "cow-hiding", "daemon", "cow", "dragon", "dragon-and-cow", "duck", "elephant", "elephant-in-snake", "eyes", "flaming-sheep", "fox", "ghostbusters", "gnu", "hellokitty", "kangaroo", "kiss", "koala", "kosh", "luke-koala", "mech-and-cow", "milk", "moofasa", "moose", "pony", "pony-smaller", "ren", "sheep", "skeleton", "snowman", "stegosaurus", "stimpy", "suse", "three-eyed-cow", "turkey", "turtle", "tux", "unipony", "small-unipony", "vader", "vader-koala", "www" ]
 
-charMenu = RadioMenu(charList, pagesize=8)
-textTypeQuery = RadioMenu(fortuneQuery, pagesize=2)
-TerminalMenus.config(charset=:unicode)
-b = request("Choose One: ", textTypeQuery)
-a = request("Pick a character: ", charMenu)
+charMenu = RadioMenu(charList, pagesize=8) # Define menu for charList
+textTypeQuery = RadioMenu(fortuneQuery, pagesize=2) # Define menu for fortuneQuery
+TerminalMenus.config(charset=:unicode) # Set menu to use unicode
+b = request("Choose One: ", textTypeQuery) # Request menu textTypeQuery
+print("\033[A\e[2K\033[A\e[2K\033[A\e[2K") # Clear previous menu
+a = request("Pick a character: ", charMenu) # Request menu charMenu
 ## Translate options the names of which are changed in the choice list.
 
 if (charList[a] == "frogs")
@@ -33,7 +34,7 @@ else
 end
 
 
-print("\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K")
+print("\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K\033[A\e[2K")
 
 if (fortuneQuery[b] == "fortune")
 	run(pipeline(`fortune`, `cowsay -f $character`))
