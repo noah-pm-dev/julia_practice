14bd0f2b0f7e160e72df2ee1f52e891fd3b32b75
FireboltAA <noman.gronckle@gmail.com>
2021-12-17 20:24:12 -0800

    more progress on alg + update .gitignore

--- prime_algorithm/algorithm.jl
+++ prime_algorithm/algorithm.jl
@@ -1,18 +1,27 @@
-open("log.txt", "w") do f
 
-	query = ARGS[1]
-	
-	queryNum = parse(Int64, query)
+query = ARGS[1]
+
+queryNum = parse(Int64, query) # Parse argument to Int64
+iter = ((queryNum - 2) * (queryNum - 2)) / 2 # Determine how many different pairs of numbers are between the query number and 1 (not including)
+println("Iterations: ", iter) # Printing amount of iteratiosn for testing purposes
+
+# Make sure only one argument is accepted
+if (length(ARGS) < 1 || length(ARGS) > 1) 
+	printstyled("Must use only one argument\n"; color = :red)
+	exit()
+end
+
+for i in iter
+	#num = string(i) * "\n"
+        #write(f, num)
 	limit = queryNum - 1
+	tmpNum1 = rand(2:limit)
+	tmpNum2 = rand(2:limit)
+	tmpString = string(tmpNum1) * ", " * string(tmpNum2)
 	
-	if (length(ARGS) < 1 || length(ARGS) > 1) 
-		printstyled("Must use only one argument\n"; color = :red)
-		exit()
+	open("log.txt", "a") do io
+		write(io, tmpString)
 	end
 	
-	for i in 2:limit
-		num = string(i) * "\n"
-	        write(f, num)
-	end
 end
 
