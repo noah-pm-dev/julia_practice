4e669e17be9f77b43cc6508d822508daad214a3f
FireboltAA <noman.gronckle@gmail.com>
2021-12-19 20:29:23 -0800

    stuff

--- prime_algorithm/algorithm.jl
+++ prime_algorithm/algorithm.jl
@@ -3,6 +3,7 @@ query = ARGS[1]
 
 queryNum = parse(Int64, query) # Parse argument to Int64
 iter = ((queryNum - 2) * (queryNum - 2)) / 2 # Determine how many different pairs of numbers are between the query number and 1 (not including)
+println("Query: ", queryNum)
 println("Iterations: ", iter) # Printing amount of iteratiosn for testing purposes
 
 # Make sure only one argument is accepted
@@ -11,14 +12,17 @@ if (length(ARGS) < 1 || length(ARGS) > 1)
 	exit()
 end
 
-for i in iter
+for i in 1:iter
 	#num = string(i) * "\n"
         #write(f, num)
 	limit = queryNum - 1
+	println(limit)
 	tmpNum1 = rand(2:limit)
+	println(tmpNum1)
 	tmpNum2 = rand(2:limit)
-	tmpString = string(tmpNum1) * ", " * string(tmpNum2)
-	
+	println(tmpNum2)
+	tmpString = string(tmpNum1) * ", " * string(tmpNum2) * "\n"
+	println(tmpString)
 	open("log.txt", "a") do io
 		write(io, tmpString)
 	end
