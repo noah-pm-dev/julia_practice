388374aa702f9dac6390a9af64c5fbf02ad388d2
FireboltAA <noman.gronckle@gmail.com>
2022-01-08 19:35:41 -0800

    remove commented code I forgot about

--- prime_algorithm/algorithm.jl
+++ prime_algorithm/algorithm.jl
@@ -50,35 +50,3 @@ for i in 2:(startPoint + 1)
 
 end
 
-
-
-
-
-
-
-
-#iter = ((queryNum - 2) * (queryNum - 2)) / 2 # Determine how many different pairs of numbers are between the query number and 1 (not including)
-#println("Query: ", queryNum)
-#println("Iterations: ", iter) # Printing amount of iteratiosn for testing purposes
-#sleep(5)
-# Make sure only one argument is accepted
-
-
-#=
-for i in 1:iter
-	#num = string(i) * "\n"
-        #write(f, num)
-	limit = queryNum - 1
-	println(limit)
-	tmpNum1 = rand(2:limit)
-	println(tmpNum1)
-	tmpNum2 = rand(2:limit)
-	println(tmpNum2)
-	tmpString = string(tmpNum1) * ", " * string(tmpNum2) * "\n"
-	println(tmpString)
-	open("log.txt", "a") do io
-		write(io, tmpString)
-	end
-	sleep(0.5)
-end
-=#
