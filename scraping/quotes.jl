0f3d6769f54ef4e1b9d8aa3178b95c52e14d4b4b
FireboltAA <noman.gronckle@gmail.com>
2022-01-13 21:43:32 -0800

    Progress on scraping practice

--- scraping/quotes.jl
+++ scraping/quotes.jl
@@ -1,15 +1,35 @@
 using Pkg
 Pkg.add("HTTP")
 Pkg.add("Gumbo")
+Pkg.add("Cascadia")
 
-using HTTP
-using Gumbo
+i = 1
+a = 1
 
-quotesBLOB = HTTP.get("https://quotes.toscrape.com/")
-quotesHTML = parsehtml(String(quotesBLOB.body))
+using HTTP, Gumbo, Cascadia
 
-body = quotesHTML.root[2]
 
-quote1 = body[1][2][1][1][1]
 
-println(quote1[1].text)
+
+for a in 1:10
+	pageNum = "https://quotes.toscrape.com/page/$a"
+	pageGet = HTTP.get(pageNum)
+	page = parsehtml(String(pageGet.body))
+	body = page.root[2]
+	selector = Selector("div.quote")
+	quotes = eachmatch(selector, body)
+	printstyled("Listing quotes on page: $a\n"; color = :green)
+	for i in eachindex(quotes)
+		println("===============")
+		
+		a = Selector("span.text")
+		_quote = Gumbo.text(eachmatch(a, quotes[i])[1])		
+		printstyled("Quote:\n$_quote\n"; color = :blue)
+
+		b = Selector("small.author")
+		author = Gumbo.text(eachmatch(b, quotes[i])[1])
+		printstyled("By: $author\n"; color = :blue)
+	
+		println("===============")
+	end
+end
