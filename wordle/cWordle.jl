f48b4138dfa1d14f2845ab0c184cb3e41e2a40f7
HuoKnight <noman.gronckle@gmail.com>
2022-03-29 22:25:50 -0700

    add 6 tries, highlighting should be correct

--- wordle/cWordle.jl
+++ wordle/cWordle.jl
@@ -1,14 +1,44 @@
 using Printf
 
+
+function greenPass(input)
+	for i in 1:5
+		if input[i] == testInput[i]
+			lettersChecked["$(input[i])"] += 1
+			push!(greenIndex, i)
+			@printf("\u001b[42m%s\u001b[49m", input[i])
+		else
+			@printf("\u001b[1C")
+		end
+	end
+end
+
+function wrongPass(input)
+	@printf("\u001b[5D")
+	for i in 1:5
+		if indexin(i, greenIndex) .== nothing
+			if occursin(input[i], join(testInput)) && lettersChecked["$(input[i])"] < lettersUsed["$(input[i])"]
+				lettersChecked["$(input[i])"] += 1
+				@printf("\u001b[43m%s\u001b[49m", input[i])
+			else
+				@printf("\u001b[100m%s\u001b[49m", input[i])
+			end
+		else
+			@printf("\u001b[1C")
+		end
+	end
+end
+
+
 testInput = [char for char in readline()]
-input = [char for char in readline()]
+#input = [char for char in readline()]
 lettersUsed = Dict{String, Any}()
 lettersChecked = Dict{String, Any}()
 greenIndex = []
 
 
 #=
-Thes blocks create two dicts. lettersUsed contains the number of each letter in the answer word,
+These blocks create two dicts. lettersUsed contains the number of each letter in the answer word,
 while lettersChecked will contain how many times a letter in the input has been checked.
 This is to prevent multiple of one letter in the input being highlighted yellow even if there is only one occurence of it in the answer.
 =#
@@ -22,8 +52,21 @@ for i in testInput
 end
 ##
 
-print("\u001b[1F")
+#print("\u001b[1F")
 
+
+for i in 1:6
+	input = [char for char in readline()]
+	print("\u001b[1F")
+	greenPass(input)
+	wrongPass(input)
+	println()
+	global greenIndex = []
+end
+
+
+
+#=
 for i in 1:5
 	if input[i] == testInput[i]
 		lettersChecked["$(input[i])"] += 1
@@ -48,7 +91,11 @@ for i in 1:5
 		@printf("\u001b[1C")
 	end
 end
-println("\n", indexin(1, greenIndex))
-println(greenIndex)
+=#
+
+
+
+#println("\n", indexin(1, greenIndex))
+#println(greenIndex)
 println("\n", lettersUsed, "\n", lettersChecked)
 #test w/ soaps, spool
